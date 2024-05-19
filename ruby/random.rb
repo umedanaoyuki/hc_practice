1ac6b297b7adc6379fa8a56aa799bf1b4e3c5d37
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-05-20 01:57:47 +0900

    printの置き換え＆sort（配列の並び替え）の追加

--- ruby/random.rb
+++ ruby/random.rb
@@ -4,24 +4,25 @@
 group = %w[A B C D E F]
 shuffled_group = group.shuffle
 
-# puts shuffledGroup
+# p shuffled_group
 
 # １つ目のグループ（配列）の長さをランダムで決定
 key = [2, 3].shuffle
 
-# puts key[0]
+# p key[0]
+# p shuffled_group[0]
 
 # １つ目のグループの長さ
 group_one_length = key[0]
 
 # １つ目のグループの配列作成
-group_one = shuffled_group[0..group_one_length - 1]
+group_one = shuffled_group[0..group_one_length - 1].sort
 
 # ２つ目のグループの配列作成
-group_two = shuffled_group[group_one_length..5]
+group_two = shuffled_group[group_one_length..5].sort
 
-puts '1つ目のグループ'
-puts group_one
+p '1つ目のグループ'
+p group_one
 
-puts '2つ目のグループ'
-puts group_two
+p '2つ目のグループ'
+p group_two
