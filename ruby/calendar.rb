77940acc9738227090c44361f9ebbf115e06f9af
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-05-23 01:02:48 +0900

    カレンダーの表紙

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -4,30 +4,14 @@ require 'date'
 
 date = Date.today
 # カレンダーの表紙
-puts "#{date.strftime('%B')} #{date.year}"
+puts "      #{date.strftime('%B')} #{date.year}"
 
-week_day = ['月', '火', '水', '木', '金', '土', '日']
+week_day = %w(日 月 火 水 木 金 土)
 
-# p date.strftime('%a')
-p week_day[date.wday - 1]
+# 曜日を表示
+week_day.each do |wday|
+  print "#{wday} "
+end
 
-# a = Date.new(1993, 2, 24)
-# p a
-# b = Date.parse('1993-02-25')
-# p b
-# b += 10
-
-# p a
-# p b
-
-# p b - a
-# #=> 10
-
-# p b.year
-# #=> 1993
-
-# p b.strftime('%a')
-# #=> "Sat"
-
-# yesterday = Date.today - 1
-# p yesterday
+print "\n"
+print 'テスト'
