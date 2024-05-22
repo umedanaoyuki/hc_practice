2373446f227c9863413d86e5a1a3205f6a12662c
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-05-23 01:48:29 +0900

    初日、最終日、初日の曜日

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -4,7 +4,8 @@ require 'date'
 
 date = Date.today
 # カレンダーの表紙
-puts "      #{date.strftime('%B')} #{date.year}"
+month = date.strftime('%m')
+puts "      #{month} #{date.year}"
 
 week_day = %w(日 月 火 水 木 金 土)
 
@@ -14,4 +15,13 @@ week_day.each do |wday|
 end
 
 print "\n"
-print 'テスト'
+p '該当月の初日'
+first_date = Date.new(date.year, month.to_i, 1)
+p first_date
+
+p '初日の曜日'
+puts week_day[first_date.wday]
+
+p '該当月の最終日'
+last_date = Date.new(date.year, month.to_i, -1)
+p last_date
