87ff6d5c68d4ea85bdcaa31697bcb8837bb41c74
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-05-24 00:54:12 +0900

    上段部の完成

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -5,7 +5,7 @@ require 'date'
 date = Date.today
 # カレンダーの表紙
 month = date.strftime('%m')
-puts "      #{month} #{date.year}"
+puts "      #{month.to_i}月 #{date.year}"
 
 week_day = %w(日 月 火 水 木 金 土)
 
@@ -19,9 +19,13 @@ p '該当月の初日'
 first_date = Date.new(date.year, month.to_i, 1)
 p first_date
 
-p '初日の曜日'
+p '該当月の初日の曜日'
 puts week_day[first_date.wday]
 
 p '該当月の最終日'
 last_date = Date.new(date.year, month.to_i, -1)
 p last_date
+
+p 'その月の日数'
+total_days = last_date - first_date
+p total_days.to_i
