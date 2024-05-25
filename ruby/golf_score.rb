f77fe9d5d7ec6a1a289f585ae6d19562ac28dd19
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-05-25 13:38:07 +0900

    場合分けのロジック（途中まで）

--- ruby/golf_score.rb
+++ ruby/golf_score.rb
@@ -6,12 +6,42 @@ input_line = gets.to_i
 # 2番目の標準入力を受け取る変数
 numbered_s = 0
 
-input_line.times do
+# input_line.times do
+
+temp_number = 1
+
+temp_number.times do
   s = gets.chomp.split(' ')
   # 2番目に入力した文字をintegerに変換
   numbered_s = s[0].to_i
 end
 
-if input_line == numbered_s
+puts '結果出力'
+
+puts input_line - numbered_s
+
+# 結果 - 規定打数
+cal_number = numbered_s - input_line
+
+case cal_number
+when 1
+  puts 'ホールインワン'
+when input_line
   puts 'パー'
+when input_line - 1
+  puts 'バーディ'
+when input_line - 2
+  puts 'イーグル'
+when input_line - 3
+  puts 'アルバトロス'
+when input_line - 4
+  puts 'コンドル'
+when input_line < numbered_s
+  puts 'ボギー'
 end
+
+# ホールインワン
+# 1の場合
+
+# ボギー
+# 規定打数よりも多い場合
