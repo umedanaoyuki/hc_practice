c90ebca82d33dac8674c2cc39538bb91c30ebd99
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-05-25 12:28:26 +0900

    標準入出力の実装

--- ruby/golf_score.rb
+++ ruby/golf_score.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 input_line = gets.to_i
+
+# 1番目に入力した文字を出力
+puts input_line
 input_line.times do
   s = gets.chomp.split(' ')
-  print "hello = #{s[0]} , world = #{s[1]}\n"
+  # print "hello = #{s[0]} , world = #{s[1]}\n"
+
+  # 2番目に入力した文字を出力
+  p s[0]
 end
