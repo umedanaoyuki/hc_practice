fe63143e2509507bc3fbf9bbb838be109e0aa2b2
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-05-25 12:45:19 +0900

    2番目にインプットした数字をintegerで出力する

--- ruby/golf_score.rb
+++ ruby/golf_score.rb
@@ -2,12 +2,12 @@
 
 input_line = gets.to_i
 
-# 1番目に入力した文字を出力
+# 1番目に入力した文字を出力（Integer）
 puts input_line
 input_line.times do
   s = gets.chomp.split(' ')
   # print "hello = #{s[0]} , world = #{s[1]}\n"
-
-  # 2番目に入力した文字を出力
-  p s[0]
+  numbered_s = s[0].to_i
+  # 2番目に入力した文字を出力（String）
+  p numbered_s
 end
