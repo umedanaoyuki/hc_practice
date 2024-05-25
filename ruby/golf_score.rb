a77161b63dead317480a5734ef3a893f3c8ff394
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-05-25 13:01:00 +0900

    条件分岐の実装

--- ruby/golf_score.rb
+++ ruby/golf_score.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
+puts '1番目の標準入力'
 input_line = gets.to_i
 
-# 1番目に入力した文字を出力（Integer）
-puts input_line
+# 2番目の標準入力を受け取る変数
+numbered_s = 0
+
 input_line.times do
   s = gets.chomp.split(' ')
-  # print "hello = #{s[0]} , world = #{s[1]}\n"
+  # 2番目に入力した文字をintegerに変換
   numbered_s = s[0].to_i
-  # 2番目に入力した文字を出力（String）
-  p numbered_s
+end
+
+if input_line == numbered_s
+  puts 'パー'
 end
