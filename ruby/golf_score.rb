a85b779f568941a4e61e15a6c99156b93e90ce16
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-06-02 03:40:36 +0900

    ロジックの整理＆変数名の修正

--- ruby/golf_score.rb
+++ ruby/golf_score.rb
@@ -1,70 +1,42 @@
 # frozen_string_literal: true
 
-# 1番目の入力を受けとる変数
-golf_scores = gets.split(',')
-# 2番目の入力を受け取る
+# 1番目の入力を受けとる変数(18ホールの各ホールの規定打数)
 par_numbers = gets.split(',')
+# 2番目の入力を受け取る(プレイヤーの打数)
+golf_scores = gets.split(',')
 
 # 出力を格納する配列
-answer = []
+score_judge = []
 
 # 配列を回すための変数
 number = 0
 
 golf_scores.each do |golf_score|
+  cal_score = par_numbers[number].to_i - golf_score.to_i
+
   if golf_score.to_i == 1
-    answer.push('ホールインワン')
-  elsif golf_score.to_i == par_numbers[number].to_i
-    answer.push('パー')
-  elsif par_numbers[number].to_i - golf_score.to_i == 1
-    answer.push('バーディ')
-  elsif  par_numbers[number].to_i - golf_score.to_i == 2
-    answer.push('イーグル')
-  elsif par_numbers[number].to_i - golf_score.to_i == 3
-    answer.push('アルバトロス')
-  elsif par_numbers[number].to_i - golf_score.to_i == 4
-    answer.push('コンドル')
+    score_judge.push('ホールインワン')
+  elsif cal_score.zero?
+    score_judge.push('パー')
+  elsif cal_score == 1
+    score_judge.push('バーディ')
+  elsif cal_score == 2
+    score_judge.push('イーグル')
+  elsif cal_score == 3
+    score_judge.push('アルバトロス')
+  elsif cal_score == 4
+    score_judge.push('コンドル')
+  elsif cal_score == -1
+    score_judge.push('ボギー')
   else
-    if (par_numbers[number].to_i - golf_score.to_i) < 1
-      answer.push("#{golf_score.to_i - par_numbers[number].to_i}ボギー")
-    else
-      answer.push('ボギー')
-    end
-
-
-
-
-
-# golf_scores.each do |golf_score|
-#   case golf_score.to_i
-#   when 1
-#     answer.push('ホールインワン')
-#   when par_numbers[number].to_i
-#     answer.push('パー')
-#   when par_numbers[number].to_i - 1
-#     answer.push('バーディ')
-#   when par_numbers[number].to_i - 2
-#     answer.push('イーグル')
-#   when par_numbers[number].to_i - 3
-#     answer.push('アルバトロス')
-#   when par_numbers[number].to_i - 4
-#     answer.push('コンドル')
-#   else
-#     # when (golf_score.to_i - par_numbers[number].to_i).positive?
-#     if (golf_score.to_i - par_numbers[number].to_i) > 1
-#       answer.push("#{golf_score.to_i - par_numbers[number].to_i}ボギー")
-#     else
-#       answer.push('ボギー')
-#     end
-
-
+    score_judge.push("#{cal_score}ボギー")
   end
   number += 1
 end
 
-array_count = answer.length
+array_count = score_judge.length
 for i in 0..array_count.to_i - 2
-  print "#{answer[i]},"
+  print "#{score_judge[i]},"
 end
 
-print answer[array_count - 1]
+print score_judge[array_count - 1]
