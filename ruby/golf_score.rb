8e03bfc4b50e39cbd42aea2963490fbcd28031d2
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-06-02 03:18:17 +0900

    ロジックの書き直し（if文を使用）

--- ruby/golf_score.rb
+++ ruby/golf_score.rb
@@ -1,34 +1,63 @@
 # frozen_string_literal: true
 
-# 1番目の標準入力を受けとる変数
+# 1番目の入力を受けとる変数
 golf_scores = gets.split(',')
-# 2番目の標準入力を受け取る
+# 2番目の入力を受け取る
 par_numbers = gets.split(',')
 
+# 出力を格納する配列
 answer = []
 
+# 配列を回すための変数
 number = 0
+
 golf_scores.each do |golf_score|
-  case golf_score.to_i
-  when 1
+  if golf_score.to_i == 1
     answer.push('ホールインワン')
-  when par_numbers[number].to_i
+  elsif golf_score.to_i == par_numbers[number].to_i
     answer.push('パー')
-  when par_numbers[number].to_i - 1
+  elsif par_numbers[number].to_i - golf_score.to_i == 1
     answer.push('バーディ')
-  when par_numbers[number].to_i - 2
+  elsif  par_numbers[number].to_i - golf_score.to_i == 2
     answer.push('イーグル')
-  when par_numbers[number].to_i - 3
+  elsif par_numbers[number].to_i - golf_score.to_i == 3
     answer.push('アルバトロス')
-  when par_numbers[number].to_i - 4
+  elsif par_numbers[number].to_i - golf_score.to_i == 4
     answer.push('コンドル')
   else
-    # when (golf_score.to_i - par_numbers[number].to_i).positive?
-    if (golf_score.to_i - par_numbers[number].to_i) > 1
+    if (par_numbers[number].to_i - golf_score.to_i) < 1
       answer.push("#{golf_score.to_i - par_numbers[number].to_i}ボギー")
     else
       answer.push('ボギー')
     end
+
+
+
+
+
+# golf_scores.each do |golf_score|
+#   case golf_score.to_i
+#   when 1
+#     answer.push('ホールインワン')
+#   when par_numbers[number].to_i
+#     answer.push('パー')
+#   when par_numbers[number].to_i - 1
+#     answer.push('バーディ')
+#   when par_numbers[number].to_i - 2
+#     answer.push('イーグル')
+#   when par_numbers[number].to_i - 3
+#     answer.push('アルバトロス')
+#   when par_numbers[number].to_i - 4
+#     answer.push('コンドル')
+#   else
+#     # when (golf_score.to_i - par_numbers[number].to_i).positive?
+#     if (golf_score.to_i - par_numbers[number].to_i) > 1
+#       answer.push("#{golf_score.to_i - par_numbers[number].to_i}ボギー")
+#     else
+#       answer.push('ボギー')
+#     end
+
+
   end
   number += 1
 end
