934da013e12782f2cf5f3463f9ad883644cc61b1
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-06-06 17:38:42 +0900

    コメントの修正

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -27,7 +27,7 @@ else
   exit
 end
 
-# カレンダーの上部を表示する
+# カレンダーの上部を表示するメソッド
 def show_calender_upper_area(date)
   # カレンダーの表紙
   calendar_cover_month = date.strftime('%m').to_i
@@ -35,21 +35,24 @@ def show_calender_upper_area(date)
   calendar_cover_space = '      '
   puts "#{calendar_cover_space}#{calendar_cover_month}月 #{date.year}"
 
+  # 月曜日始まり
   week_day = %w(月 火 水 木 金 土 日)
   puts week_day.join(' ')
 end
 
-# カレンダーの日付の表示のメソッド
+# カレンダーの日付箇所の表示メソッド
 def show_calendar_dates(first_date, last_date)
   # 該当月の初日の曜日を取得
   # 0:日曜日, 1:月曜日, 2:火曜日, 3:水曜日, 4:木曜日, 5:金曜日, 6:土曜日
   week_day_of_first_date = first_date.wday.to_i
 
-  # 月の最初の日曜日の日付を求める
-  # 月の最初の日曜日の日付 = 8 - 該当月の初日の曜日の番号
   # 月の最初の日曜日の初期値
   first_sunday = 1
+
+  # 該当月の最初の日曜日の日付を求める
+  # 月初が日曜日でない場合の処理（月初が日曜日の場合は、first_sundayは1のまま）
   if week_day_of_first_date != 0
+    # 月の最初の日曜日の日付 = 8 - 該当月の初日の曜日の番号
     first_sunday = 8 - week_day_of_first_date
   end
 
