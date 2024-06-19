8f685c00c35dfe51ed59745f3e044097c34585d9
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-06-19 20:07:56 +0900

    ハッシュを使って、空の配列に結果を入れる（複数ボギーについては別途実装）

--- ruby/golf_score.rb
+++ ruby/golf_score.rb
@@ -33,33 +33,40 @@ SCORE_MAPPING = {
     "-1" => "ボギー"
 }
 
-puts 'print'
-puts SCORE_MAPPING['4']
+# puts 'print'
+# puts SCORE_MAPPING[4.to_s]
 
 golf_scores.each do |golf_score|
   # ゴルフの判定を行うための変数
   cal_score = par_numbers[number].to_i - golf_score.to_i
 
-  if par_numbers[number].to_i == 5 && golf_score.to_i == 1
-    score_judge.push('コンドル')
-  elsif golf_score.to_i == 1
-    score_judge.push('ホールインワン')
-  elsif cal_score.zero?
-    score_judge.push('パー')
-  elsif cal_score == 1
-    score_judge.push('バーディ')
-  elsif cal_score == 2
-    score_judge.push('イーグル')
-  elsif cal_score == 3
-    score_judge.push('アルバトロス')
-  elsif cal_score == 4
-    score_judge.push('コンドル')
-  elsif cal_score == - 1
-    score_judge.push('ボギー')
-  else
-    score_judge.push("#{golf_score.to_i - par_numbers[number].to_i}ボギー")
-  end
+  # if golf_score.to_i == 1
+  #   score_judge.push('ホールインワン')
+  # end
+
+  score_judge.push(SCORE_MAPPING[cal_score.to_s])
   number += 1
+
+#   if par_numbers[number].to_i == 5 && golf_score.to_i == 1
+#     score_judge.push('コンドル')
+#   elsif golf_score.to_i == 1
+#     score_judge.push('ホールインワン')
+#   elsif cal_score.zero?
+#     score_judge.push('パー')
+#   elsif cal_score == 1
+#     score_judge.push('バーディ')
+#   elsif cal_score == 2
+#     score_judge.push('イーグル')
+#   elsif cal_score == 3
+#     score_judge.push('アルバトロス')
+#   elsif cal_score == 4
+#     score_judge.push('コンドル')
+#   elsif cal_score == - 1
+#     score_judge.push('ボギー')
+#   else
+#     score_judge.push("#{golf_score.to_i - par_numbers[number].to_i}ボギー")
+#   end
+#   number += 1
 end
 
 score_judge.each_with_index do |score, i|
