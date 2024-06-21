d669b01728591c0415463236e8ef1d76ce8cdb67
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-06-22 01:12:02 +0900

    購入の場合分けの実装

--- ruby/vending_machine/Machine.rb
+++ ruby/vending_machine/Machine.rb
@@ -1,6 +1,7 @@
 require './Suica'
 require './Juice'
 
+# 販売機クラス
 class Machine
   attr_reader :inventory, :sales
 
@@ -9,23 +10,21 @@ class Machine
     @sales = sales
   end
 
-  suica = Suica.new
-  # puts suica.balance
-
-  juice = Juice.new
-  # puts juice.price
-
 # 自動販売機はジュースの在庫を減らす
 # 売り上げ金額を増やす
 # Suicaのチャージ残高を減らす
 def purchase(suica, juice)
+  if @inventory == 0
+    raise "#{juice}の在庫がありません"
+  end
+
   if suica.balance >= juice.price
     @inventory -= 1
     @sales += juice.price
     suica.change_balance(juice.price)
-    puts 'OK'
+    puts "#{juice.name}の購入完了"
   else
-  puts 'NG'
+    raise "Suicaの残高が足りません"
   end
 end
 end
@@ -34,6 +33,9 @@ suica = Suica.new
 juice = Juice.new
 machine = Machine.new
 machine.purchase(suica, juice)
+
+# 現在の売上金額合計
+puts "現在の売上金額合計は#{machine.sales}円です"
 
 # puts '在庫確認'
 # puts machine.inventory
