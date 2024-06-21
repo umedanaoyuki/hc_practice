9f69793d099af2c37c44e173e6581498cb69b4d9
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-06-21 21:58:42 +0900

    Suicaクラスの軽微な変更

--- ruby/vending_machine/Suica.rb
+++ ruby/vending_machine/Suica.rb
@@ -5,29 +5,34 @@
 
 # Suica
 class Suica
-  attr_reader :deposit
+  attr_reader :balance
 
-  def initialize(deposit = 500)
-    @deposit = deposit
+  def initialize(balance = 0)
+    # デポジット500円
+    @balance = 500 + balance
   end
 
   # def show_deposit
   #   puts "デポジットは#{@deposit}円です"
   # end
 
-  def show_balance(charge)
+  def charge(charge)
     if charge < 100
       raise "100円未満はチャージできません"
     end
 
-    balance = deposit + charge
+    @balance = balance + charge
+  end
+
+  def show_balance
     puts "Suica残高は#{balance}円です"
   end
 end
 
 suica = Suica.new
 
+suica.charge(500)
 puts '残高表示'
-suica.show_balance(105)
+suica.show_balance
 # puts 'depositの表示'
 # suica.show_deposit
