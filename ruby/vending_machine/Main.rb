9204978c51ca583aaa794a8438604f7f74910f1c
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-06-22 14:31:29 +0900

    Mainでのロジック整理

--- ruby/vending_machine/Main.rb
+++ ruby/vending_machine/Main.rb
@@ -1,24 +1,30 @@
-# 自動販売機はペプシが購入できるかどうかを取得できる。
-# ジュース値段以上のチャージ残高がある条件下で購入操作を行うと以下の動作をする
-# 自動販売機はジュースの在庫を減らす
-# 売り上げ金額を増やす
-# Suicaのチャージ残高を減らす
-# チャージ残高が足りない場合もしくは在庫がない場合、購入操作を行った場合は例外を発生させる
-# 自動販売機は現在の売上金額を取得できる
 
 require './Suica'
 require './Juice'
 require './Machine'
 
 suica = Suica.new
+machine = Machine.new
 pepsi = Juice.new('ペプシ', 150, 5)
 monster = Juice.new('モンスター', 230, 5)
 irohas = Juice.new('いろはす', 120, 5)
-machine = Machine.new
+
+# ペプシ1本購入
 machine.purchase(suica, pepsi)
-
 # 現在の売上金額合計
-puts "現在の売上金額合計は#{machine.sales}円です"
+puts "現在の販売機の売上金額合計は#{machine.sales}円です"
 
-# puts '在庫確認'
-# puts machine.inventory
+# モンスター1本購入
+machine.purchase(suica, monster)
+# 現在の売上金額合計
+puts "現在の販売機の売上金額合計は#{machine.sales}円です"
+
+# いろはす1本購入
+machine.purchase(suica, irohas)
+# 現在の売上金額合計
+puts "現在の販売機の売上金額合計は#{machine.sales}円です"
+
+puts '在庫確認'
+puts "#{pepsi.name}の在庫は#{pepsi.inventory}本です"
+puts "#{monster.name}の在庫は#{monster.inventory}本です"
+puts "#{irohas.name}の在庫は#{irohas.inventory}本です"
