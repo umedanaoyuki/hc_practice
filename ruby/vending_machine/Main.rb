0555fa8a932ae5cbe5f98f507280bc61bf0c28d1
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-06-23 00:16:31 +0900

    すべてのジュースを購入する処理を記載

--- ruby/vending_machine/Main.rb
+++ ruby/vending_machine/Main.rb
@@ -21,11 +21,11 @@ irohas = Juice.new('いろはす', 120)
 # puts "現在の販売機の売上金額合計は#{machine.sales}円です"
 
 # 在庫を追加
+puts '在庫を追加'
 machine.add_juice(pepsi, 5)
 # puts machine.inventory[pepsi][:stock]
-puts '在庫を追加'
-machine.add_juice(monster, 6)
-machine.add_juice(irohas, 6)
+machine.add_juice(monster, 5)
+machine.add_juice(irohas, 5)
 
 # puts "検証スタート"
 # puts machine.inventory[pepsi][:juice].name
@@ -36,24 +36,24 @@ machine.add_juice(irohas, 6)
 # puts machine.inventory[irohas][:stock]
 # puts "終了"
 
+# ペプシ1本購入
+machine.purchase(suica, pepsi)
+
 # # モンスター1本購入
 machine.purchase(suica, monster)
 # # 現在の売上金額合計
 puts "現在の販売機の売上金額合計は#{machine.sales}円です"
 
 # # いろはす1本購入
-# machine.purchase(suica, irohas)
+machine.purchase(suica, irohas)
 # # 現在の売上金額合計
-# puts "現在の販売機の売上金額合計は#{machine.sales}円です"
+puts "現在の販売機の売上金額合計は#{machine.sales}円です"
 
 # # 200円のチャージ
-# suica.charge(200)
-# puts "Suica残高は#{suica.show_balance}円です"
-
-# # 在庫補填をする
-# pepsi.add_inventory(5)
+suica.charge(200)
+puts "Suica残高は#{suica.show_balance}円です"
 
 # puts '在庫確認'
-# puts "#{pepsi.name}の在庫は#{pepsi.inventory}本です"
-# puts "#{monster.name}の在庫は#{monster.inventory}本です"
-# puts "#{irohas.name}の在庫は#{irohas.inventory}本です"
+puts "#{pepsi.name}の在庫は#{machine.inventory[pepsi][:stock]}本です"
+puts "#{monster.name}の在庫は#{machine.inventory[monster][:stock]}本です"
+puts "#{irohas.name}の在庫は#{machine.inventory[irohas][:stock]}本です"
