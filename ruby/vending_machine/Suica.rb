dbdea489a6e4fafd9b55c265b8fba77697e29cf9
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-06-23 14:03:15 +0900

    ドキュメントの追加

--- ruby/vending_machine/Suica.rb
+++ ruby/vending_machine/Suica.rb
@@ -7,6 +7,7 @@ class Suica
     @balance = 500 + balance
   end
 
+  # Suicaに現金をチャージするメソッド
   def charge(charge)
     raise '100円未満はチャージできません。' if charge < 100
 
@@ -14,10 +15,12 @@ class Suica
     puts "#{charge}円のチャージ完了しました"
   end
 
+  # 現在のSuica残高を取得するメソッド
   def show_balance
     balance
   end
 
+  # 飲み物購入時にSuica残高を減らすメソッド
   def change_balance(amount)
     @balance -= amount
   end
