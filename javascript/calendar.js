f18b46675aaaa3130e3325d49a2cb56efcdbd95b
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-07-19 18:29:15 +0900

    8月までのカレンダー完成（整形前）

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -1,27 +1,28 @@
-console.log("hello world");
-
-// 配列の3番目（[2]）以降で引数を受け取れる
+// 配列の3番目（[2]）以降で引数を受け取れ
 const inputNumber = process.argv[2];
-console.log(inputNumber);
+// console.log(inputNumber);
 
 // 今年の年の取得
 const date = new Date();
 const year = date.getFullYear();
 // カレンダーの表示する月を決める（引数がない場合、現在の月を表示）
 const monthToDisplay = inputNumber ? inputNumber : date.getMonth() + 1;
+// console.log(monthToDisplay);
 
-// カレンダーに表示する月の最初の日を求める
+// カレンダーに表示する月の最初の日（月初）を求める
 const firstDate = new Date();
 firstDate.setFullYear(year, 0);
 firstDate.setMonth(monthToDisplay, 0);
 firstDate.setDate(1);
-console.log("月初:" + firstDate.getDay());
+console.log(firstDate);
+console.log("曜日");
+console.log(firstDate.getDay());
 
 // カレンダーに表示する月の最終日を求める
 const lastDate = new Date();
 lastDate.setFullYear(year, 0);
 lastDate.setMonth(monthToDisplay, 0);
-console.log(lastDate);
+// console.log(lastDate);
 
 // カレンダーの上部を表示するメソッド
 const showUpperArea = (monthToDisplay, year) => {
@@ -34,7 +35,7 @@ const showUpperArea = (monthToDisplay, year) => {
 const showDateArea = (firstDate, lastDate) => {
   // 該当月の初日の曜日を取得
   // 0:日曜日, 1:月曜日, 2:火曜日, 3:水曜日, 4:木曜日, 5:金曜日, 6:土曜日
-  const weekDay = firstDate.getDate();
+  const weekDay = firstDate.getDay();
 
   // 該当月の最初の日曜日の日付を求める
   if (weekDay !== 0) {
@@ -49,28 +50,19 @@ const showDateArea = (firstDate, lastDate) => {
     // console.log(initialEmpty);
     // console.log("終了");
 
-    // console.log(lastDate.getDate());
     // 日曜日になったら改行して日付を表示
-    for (let i = firstDate.getDay(); i <= lastDate.getDate(); i++) {
-      // console.log(`テスト ${i}`)
-      // 改行する場合
-      // process.stdout.write("HelloWorld\n");
-      // 改行しない場合
-      // process.stdout.write(i.toString());
-      // console.log(i.toString());
+    // lastDate.getDate()
+    // const testDate = firstDate.getDay();
+    for (let i = 1; i <= lastDate.getDate(); i++) {
+      // console.log(firstDate.getDay());
 
-      // console.log("FIRST DATE " + firstDate);
-
-      // console.log(firstDate.getDate());
-
-      if (firstDate.getDate() === 7) {
+      if (firstDate.getDay() === 0) {
         // console.log("改行");
         console.log(i.toString());
       } else {
         process.stdout.write(i.toString());
       }
-
-      firstDate.setDate(firstDate.getDay() + 1);
+      firstDate.setDate(firstDate.getDate() + 1);
     }
   }
 };
