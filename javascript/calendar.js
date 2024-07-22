535b6e839ea5444f00948da62305367e9518b798
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-07-22 16:08:52 +0900

    三項演算子を使用して条件式を書き直し

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -37,17 +37,23 @@ switch (monthArgument) {
 // カレンダーの表示する月を決める（引数がない場合、現在の月を表示）
 const monthToDisplay = inputNumber
   ? parseInt(inputNumber)
-  : parseInt(date.getMonth() + 1);
+  : parseInt(date.getMonth()) + 1;
+
+// console.log(monthToDisplay);
 
 // カレンダーに表示する月の最初の日（月初）を求める
 const firstDate = new Date();
 firstDate.setFullYear(year, 0);
-firstDate.setMonth(monthToDisplay, 1);
+firstDate.setMonth(monthToDisplay, 0);
+firstDate.setDate(1);
+
+console.log(`firstDate:${firstDate}`);
 
 // カレンダーに表示する月の最終日を求める
 const lastDate = new Date();
 lastDate.setFullYear(year, 0);
 lastDate.setMonth(monthToDisplay, 0);
+console.log(`lastDate:${lastDate}`);
 
 /**
  * カレンダーの上部（タイトルと曜日）を表示するメソッド
@@ -68,45 +74,56 @@ const showDateArea = (firstDate, lastDate) => {
   // 該当月の初日の曜日を取得
   // 0:日曜日, 1:月曜日, 2:火曜日, 3:水曜日, 4:木曜日, 5:金曜日, 6:土曜日
   const weekDay = parseInt(firstDate.getDay());
+  console.log(weekDay);
+
+  const spaceCount = weekDay === 0 ? 18 : weekDay * 3;
+  console.log(`spaceCountは:${spaceCount}`);
+
+  const separator = weekDay === 0 ? 18 : spaceCount - 3;
+  console.log(`separatorは${separator}`);
+  process.stdout.write(" ".repeat(separator));
+  // console.log("テスト");
 
   // 該当月が日曜日以外から始まる場合
-  if (weekDay !== 0) {
-    // 該当月が月曜日以外から始まる場合
-    if (weekDay !== 1) {
-      // 月初の日付の前に表示させる「空欄」の算出
-      const separator = " ".toString().padStart(3 * (weekDay - 1), " ");
-      process.stdout.write(separator);
-    }
+  // if (weekDay !== 0) {
+  // process.stdout.write(" ".repeat(spaceCount));
+  // 該当月が月曜日以外から始まる場合
+  // if (weekDay !== 1) {
+  //   // 月初の日付の前に表示させる「空欄」の算出
+  //   const separator = " ".toString().padStart(3 * (weekDay - 1), " ");
+  //   process.stdout.write(separator);
+  // }
 
-    for (let i = 1; i <= lastDate.getDate(); i++) {
-      // 日曜日になったら改行
-      if (firstDate.getDay() === 0) {
-        console.log(i.toString().padStart(2, " "));
-      } else {
-        process.stdout.write(i.toString().padStart(2, " "));
-        // 日付と日付の間の空欄
-        process.stdout.write(" ");
-      }
-      firstDate.setDate(firstDate.getDate() + 1);
+  for (let i = 1; i <= lastDate.getDate(); i++) {
+    // 日曜日になったら改行
+    if (firstDate.getDay() === 0) {
+      console.log(i.toString().padStart(2, " "));
+    } else {
+      process.stdout.write(i.toString().padStart(2, " "));
+      // 日付と日付の間の空欄
+      process.stdout.write(" ");
     }
-  } else {
-    // console.log("こっち");
-    // 該当月が日曜日から始まる場合
-
-    const spaceCount = (weekDay === 0 ? 6 : weekDay) * 3;
-    // const separator = " ".toString().padStart(spaceCount, " ");
-    process.stdout.write(" ".repeat(spaceCount));
-
-    for (let i = 1; i <= lastDate.getDate(); i++) {
-      if (firstDate.getDay() === 0) {
-        console.log(i.toString().padStart(2, " "));
-      } else {
-        process.stdout.write(i.toString().padStart(2, " "));
-        process.stdout.write(" ");
-      }
-      firstDate.setDate(firstDate.getDate() + 1);
-    }
+    firstDate.setDate(firstDate.getDate() + 1);
   }
+  // } else {
+  // const test = 1;
+  // process.stdout.write(test.toString().padStart(2, " "));
+  // console.log("こっち");
+  // 該当月が日曜日から始まる場合
+  // const spaceCount = (weekDay === 0 ? 6 : weekDay) * 3;
+  // const separator = " ".toString().padStart(spaceCount, " ");
+  // process.stdout.write(" ".repeat(spaceCount));
+  // console.log("テスト");
+  // for (let i = 1; i <= lastDate.getDate(); i++) {
+  //   if (firstDate.getDay() === 0) {
+  //     console.log(i.toString().padStart(2, " "));
+  //   } else {
+  //     process.stdout.write(i.toString().padStart(2, " "));
+  //     process.stdout.write(" ");
+  //   }
+  //   firstDate.setDate(firstDate.getDate() + 1);
+  // }
+  // }
 };
 
 showUpperArea(monthToDisplay, year);
