c8e4eeabe716a71056241790755a9ef268c8e97e
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-07-28 22:48:51 +0900

    タスクを追加したら未完了のタスクの数を表示

--- javascript/todo/main.js
+++ javascript/todo/main.js
@@ -1,11 +1,24 @@
+// TODOリストを格納する配列
 let todoList = [];
 const addTask = document.getElementById("addTask");
 addTask.addEventListener("click", addTodoItem);
+
+// すべてのタスクの数
+let totalCount = todoList.length;
+
+// 完了済タスクの数
+let tasksCompleted = 0;
+
+// 未完了のタスクの数
+// let tasksNotCompleted = totalCount - tasksCompleted;
+let tasksNotCompleted = totalCount - tasksCompleted;
 
 function addTodoItem() {
   console.log("ボタン押下");
   let item = document.getElementById("todoInput").value;
   todoList.push(item);
+
+  // console.log(todoList);
 
   let list = document.getElementById("todoList");
   let listItem = document.createElement("div");
@@ -14,13 +27,27 @@ function addTodoItem() {
   let checkbox = document.createElement("input");
   checkbox.type = "checkbox";
 
+  // console.log(listItem);
+
   checkbox.onclick = function () {
     if (checkbox.checked) {
       itemText.style.textDecoration = "line-through";
       listItem.classList.add("checked");
+
+      // console.log("listItem" + listItem.classList);
+      tasksCompleted += 1;
+      // console.log(tasksCompleted);
+
+      const completedTasks = document.getElementById("completed-tasks");
+      completedTasks.textContent = tasksCompleted;
+
+      const notCompletedTasks = document.getElementById("not-completed-tasks");
+      notCompletedTasks.textContent = tasksNotCompleted;
     } else {
       itemText.style.textDecoration = "none";
       listItem.classList.remove("checked");
+      tasksCompleted -= 1;
+      // console.log("テスト" + tasksCompleted);
     }
   };
 
@@ -32,19 +59,13 @@ function addTodoItem() {
   let buttonsDiv = document.createElement("div");
   listItem.appendChild(buttonsDiv);
 
-  // let updateButton = document.createElement("button");
-  // updateButton.innerHTML = '<i class="update-button">保存</i>';
-  // updateButton.onclick = function () {
-  //   itemText.contentEditable = false;
-  //   todoList[todoList.indexOf(item)] = itemText.textContent;
-  // };
-
   let editButton = document.createElement("button");
   editButton.innerHTML = '<i class="edit-button">編集</i>';
   editButton.onclick = function () {
     itemText.contentEditable = true;
     itemText.focus();
 
+    // 何度も保存ボタンが表示されるようになってしまう
     let updateButton = document.createElement("button");
     updateButton.innerHTML = '<i class="update-button">保存</i>';
     console.log("保存");
@@ -70,4 +91,15 @@ function addTodoItem() {
   buttonsDiv.appendChild(deleteButton);
   list.appendChild(listItem);
   document.getElementById("todoInput").value = "";
+
+  console.log("allTaskNumber");
+  const allTaskNumber = document.getElementById("all-task-number");
+  allTaskNumber.textContent = todoList.length;
+
+  const notCompletedTasks = document.getElementById("not-completed-tasks");
+  // console.log("asksNotCompleted" + tasksNotCompleted);
+  tasksCompleted += 1;
+  notCompletedTasks.textContent = tasksCompleted;
+
+  // console.log(todoList.length);
 }
