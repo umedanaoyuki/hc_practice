bc10183bb5e5f590e0edbae88ce21b7814bb2edf
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-08-04 16:49:24 +0900

    flexboxを適用させることに成功

--- javascript/js_kimetsu_list/main.js
+++ javascript/js_kimetsu_list/main.js
@@ -10,16 +10,20 @@ fetch("https://ihatov08.github.io/kimetsu_api/api/all.json")
     for (let i = 0; i < res.length; i++) {
       console.log(res[i]);
 
+      const div = document.createElement("div");
+      div.className = "animal";
+      document.getElementsByClassName("mainDiv")[0].appendChild(div);
+
       const h1 = document.createElement("h1");
       h1.textContent = res[i].name;
-      document.getElementsByClassName("animal")[0].appendChild(h1);
+      document.getElementsByClassName("animal")[i].appendChild(h1);
 
       const p = document.createElement("p");
       p.textContent = res[i].category;
-      document.getElementsByClassName("animal")[0].appendChild(p);
+      document.getElementsByClassName("animal")[i].appendChild(p);
 
       const img = document.createElement("img");
       img.src = `https://ihatov08.github.io${res[i].image}`;
-      document.getElementsByClassName("animal")[0].appendChild(img);
+      document.getElementsByClassName("animal")[i].appendChild(img);
     }
   });
