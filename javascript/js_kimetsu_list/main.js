b90acb40084e1f33f027dc9a89bd205745d585a4
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-08-04 14:08:22 +0900

    APIのレスポンスをhtmlで表示させる処理を記載

--- javascript/js_kimetsu_list/main.js
+++ javascript/js_kimetsu_list/main.js
@@ -6,4 +6,11 @@ fetch("https://ihatov08.github.io/kimetsu_api/api/all.json")
   })
   .then((res) => {
     console.log(res);
+    const h1 = document.createElement("h1");
+    h1.textContent = res[0].name;
+    document.getElementsByClassName("animal")[0].appendChild(h1);
+
+    const p = document.createElement("p");
+    p.textContent = res[0].category;
+    document.getElementsByClassName("animal")[0].appendChild(p);
   });
