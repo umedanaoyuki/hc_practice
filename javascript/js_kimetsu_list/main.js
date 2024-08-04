6f99f0d55902c3787c5f84287ed8cb50d9581fd1
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-08-04 22:38:40 +0900

    ローディング画面を表示させる処理の関数だけ追加

--- javascript/js_kimetsu_list/main.js
+++ javascript/js_kimetsu_list/main.js
@@ -1,29 +1,42 @@
 console.log("jsスタート");
 
-fetch("https://ihatov08.github.io/kimetsu_api/api/all.json")
-  .then((res) => {
-    return res.json();
-  })
-  .then((res) => {
-    console.log(res.length);
+const fetchKimetsuApi = () => {
+  console.log("APIを叩く");
 
-    for (let i = 0; i < res.length; i++) {
-      console.log(res[i]);
+  fetch("https://ihatov08.github.io/kimetsu_api/api/all.json")
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      console.log(res.length);
 
-      const div = document.createElement("div");
-      div.className = "animal";
-      document.getElementsByClassName("mainDiv")[0].appendChild(div);
+      for (let i = 0; i < res.length; i++) {
+        console.log(res[i]);
 
-      const h1 = document.createElement("h1");
-      h1.textContent = res[i].name;
-      document.getElementsByClassName("animal")[i].appendChild(h1);
+        const div = document.createElement("div");
+        div.className = "animal";
+        document.getElementsByClassName("mainDiv")[0].appendChild(div);
 
-      const p = document.createElement("p");
-      p.textContent = res[i].category;
-      document.getElementsByClassName("animal")[i].appendChild(p);
+        const h1 = document.createElement("h1");
+        h1.textContent = res[i].name;
+        document.getElementsByClassName("animal")[i].appendChild(h1);
 
-      const img = document.createElement("img");
-      img.src = `https://ihatov08.github.io${res[i].image}`;
-      document.getElementsByClassName("animal")[i].appendChild(img);
-    }
-  });
+        const p = document.createElement("p");
+        p.textContent = res[i].category;
+        document.getElementsByClassName("animal")[i].appendChild(p);
+
+        const img = document.createElement("img");
+        img.src = `https://ihatov08.github.io${res[i].image}`;
+        document.getElementsByClassName("animal")[i].appendChild(img);
+      }
+    });
+};
+
+if (document.readyState === "loading") {
+  console.log("ローディング中");
+  // Loading hasn't finished yet
+  document.addEventListener("DOMContentLoaded", fetchKimetsuApi);
+} else {
+  // `DOMContentLoaded` has already fired
+  fetchKimetsuApi();
+}
