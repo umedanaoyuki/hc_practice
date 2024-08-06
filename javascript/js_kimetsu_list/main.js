2fcd0dd8190b395fe61f9969623d14f75ee52e84
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-08-06 23:53:21 +0900

    ローディング画面の消去

--- javascript/js_kimetsu_list/main.js
+++ javascript/js_kimetsu_list/main.js
@@ -1,33 +1,14 @@
-// console.log("jsスタート");
-
-const charactorMap = {
-  "all-charactor": "all.json",
-};
-
-let user = {
-  // ALL API
-  "all-character": "all.json",
-  // 鬼殺隊API
-  "kisatsutai-members": "kisatsutai.json",
-  // 柱API
-  "hashira-members": "hashira.json",
-  // 鬼API
-  "oni-members": "oni.json",
-};
-
 const fetchKimetsuApi = (jsonType) => {
-  // all.json
-  // /hashira.json
+  console.log("fetch");
+
   fetch(`https://ihatov08.github.io/kimetsu_api/api/${jsonType}`)
     .then((res) => {
+      const p = document.getElementsByTagName("p");
+      p.innerHTML = "";
       return res.json();
     })
     .then((res) => {
-      // console.log(res.length);
-
       for (let i = 0; i < res.length; i++) {
-        // console.log(res[i]);
-
         const div = document.createElement("div");
         div.className = "animal";
         document.getElementsByClassName("mainDiv")[0].appendChild(div);
@@ -49,13 +30,11 @@ const fetchKimetsuApi = (jsonType) => {
 
 if (document.readyState === "loading") {
   const p = document.createElement("p");
+  p.className = "test-tag";
   p.textContent = "ローディング中";
-
   document.body.appendChild(p);
+  p.innerHTML = "";
 
-  // 空欄で区切る
-  // const lines = tempText.split(" ");
-  // console.log(lines);
   setTimeout(() => fetchKimetsuApi("all.json"), 1000);
 } else {
   fetchKimetsuApi("all.json");
@@ -64,24 +43,43 @@ if (document.readyState === "loading") {
 document.querySelectorAll("input[type=radio]").forEach((element) => {
   //チェックを変更すると発生するイベントを設置
   element.addEventListener("change", () => {
+    // const p = document.getElementsByTagName("p");
+    // p.innerHTML = "";
+
     //ここにイベントの内容を記述
-    console.log("chekbox切り替え完了");
+    console.log("checkbox切り替え完了");
 
-    // 全キャラクター
-    const allCharacter = document.getElementById("all-character");
+    // const p = document.createElement("p");
+    // p.textContent = "ローディング中";
+    // document.body.appendChild(p);
 
-    //  鬼殺隊
-    const kisatsutaiMembers = document.getElementById("kisatsutai-members");
+    const temp = document.getElementById("mainDiv");
+    // temp.removeChild();
+    console.log(temp);
 
-    // 柱
-    const hashiraMembers = document.getElementById("hashira-members");
+    // 叩くAPIの種類の配列
+    // all.json
+    // kisatsutai.json
+    // hashira.json
+    // oni.json
+    const apiArray = [
+      "all-character",
+      "kisatsutai-members",
+      "hashira-members",
+      "oni-members",
+    ];
 
-    // 鬼
-    const oniMembers = document.getElementById("oni-members");
-
-    console.log(allCharacter.checked);
-    console.log(kisatsutaiMembers.checked);
-    console.log(hashiraMembers.checked);
-    console.log(oniMembers.checked);
+    for (let i = 0; i < apiArray.length; i++) {
+      const checkboxStatus = document.getElementById(apiArray[i]).checked;
+      // console.log(checkboxStatus);
+      if (checkboxStatus === true) {
+        console.log("入室");
+        // document.readyState = "loading";
+        setTimeout(() => fetchKimetsuApi("hashira.json"), 1000);
+        // fetchKimetsuApi("all.json");
+        console.log("break");
+        break;
+      }
+    }
   });
 });
