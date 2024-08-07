d8350a71e02e5101498e0125d12a1f350c6d971d
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-08-08 00:03:46 +0900

    不要なコメントの削除

--- javascript/js_kimetsu_list/main.js
+++ javascript/js_kimetsu_list/main.js
@@ -14,18 +14,13 @@ const showLoadingIcon = () => {
 const deleteLoadingIcon = () => {
   const p = document.getElementsByClassName("loadingText");
   p[0].remove();
-  // p[0].innerText = "";
 };
 
 /**
  * 鬼滅の刃APIを叩くメソッド
  */
 const fetchKimetsuApi = (jsonType) => {
-  // console.log("fetch関数");
   showLoadingIcon();
-
-  console.log("jsonType");
-  console.log(jsonType);
 
   fetch(`https://ihatov08.github.io/kimetsu_api/api/${jsonType}.json`)
     .then((res) => {
@@ -58,15 +53,10 @@ const fetchKimetsuApi = (jsonType) => {
  * 画面描画のメソッド
  */
 const renderScreen = (jsonType = "all") => {
-  // console.log("実行");
   showLoadingIcon();
   if (document.readyState === "loading") {
-    // console.log("ローディングアイコン表示");
-    // console.log('loading')
-
     setTimeout(() => fetchKimetsuApi(`${jsonType}`), 1000);
   } else {
-    // console.log("else");
     fetchKimetsuApi("all");
   }
 };
@@ -74,15 +64,11 @@ const renderScreen = (jsonType = "all") => {
 window.onload = renderScreen();
 
 document.querySelectorAll("input[type=radio]").forEach((element) => {
-  //チェックを変更すると発生するイベントを設置
   element.addEventListener("change", () => {
     showLoadingIcon();
 
-    // console.log("checkbox切り替え完了");
+    // 「animal」名の要素の配列
     const animals = document.getElementsByClassName("animal");
-
-    // console.log("5つの要素の取得");
-    // console.log(animals);
 
     while (animals.length > 0) {
       animals[0].remove();
