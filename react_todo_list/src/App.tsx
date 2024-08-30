66087f438556f19379979f58143b2d19d0eb597f
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-08-31 00:59:53 +0900

    React式のマークアップ

--- react_todo_list/src/App.tsx
+++ react_todo_list/src/App.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import "./styles.css";
 
 function App() {
+  const [incompleteTodos, setIncompleteTodos] = useState<string[]>([
+    "TODOです1",
+    "TODOです2",
+  ]);
+
+  const [completeTodos, setCompleteTodos] = useState<string[]>([
+    "TODOでした1",
+    "TODOでした2",
+  ]);
+
   return (
     <>
       <div className="input-area">
@@ -10,38 +21,28 @@ function App() {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          <li>
-            <div className="list-row">
-              <p className="todo-item">TODOです</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-          </li>
-          <li>
-            <div className="list-row">
-              <p className="todo-item">TODOです</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-          </li>
+          {incompleteTodos.map((todo) => (
+            <li key={todo}>
+              <div className="list-row">
+                <p className="todo-item">{todo}</p>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          <li>
-            <div className="list-row">
-              <p className="todo-item">TODOでした</p>
-              <button>戻す</button>
-            </div>
-          </li>
-          <li>
-            <div className="list-row">
-              <p className="todo-item">TODOでした</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-          </li>
+          {completeTodos.map((todo) => (
+            <li key={todo}>
+              <div className="list-row">
+                <p className="todo-item">{todo}</p>
+                <button>戻す</button>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </>
