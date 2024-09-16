868693c0014b1dadba8abba27fcfcc6fb42652d7
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-09-17 01:47:48 +0900

    feat: データを整形してテーブルで表示

--- react_typescript/src/App.tsx
+++ react_typescript/src/App.tsx
@@ -6,31 +6,43 @@ function App() {
   const [userListData, setUserListData] =
     useState<userListDataType[]>(USER_LIST);
 
-  console.log(userListData);
-
   const handleDisplayData = userListData.map(
     ({ id, name, role, email, age, postCode, phone, hobbies, url }) => {
       return (
-        <div key={id}>
-          <p className="">{name}</p>
-          <p className="">{role}</p>
-          <p className="">{email}</p>
-          <p className="">{age}</p>
-          <p className="">{postCode}</p>
-          <p className="">{phone}</p>
-          <p className="">
-            {hobbies.map((hobby) => {
-              return <p>{hobby}</p>;
-            })}
-          </p>
-          <p>{url}</p>
-          <p>--------------------------------</p>
-        </div>
+        <tr key={id}>
+          <th scope="row">{name}</th>
+          <td>{role}</td>
+          <td>{email}</td>
+          <td>{age}</td>
+          <td>{postCode}</td>
+          <td>{phone}</td>
+          <td>{hobbies}</td>
+          <td>{url}</td>
+        </tr>
       );
     }
   );
 
-  return <>{handleDisplayData}</>;
+  return (
+    <>
+      {" "}
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">名前</th>
+            <th scope="col">ロール</th>
+            <th scope="col">メールアドレス</th>
+            <th scope="col">年齢</th>
+            <th scope="col">郵便番号</th>
+            <th scope="col">電話番号</th>
+            <th scope="col">趣味（リスト）</th>
+            <th scope="col">URL</th>
+          </tr>
+        </thead>
+        <tbody>{handleDisplayData}</tbody>
+      </table>
+    </>
+  );
 }
 
 export default App;
