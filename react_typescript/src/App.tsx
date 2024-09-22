1aff1a73401e68f7186199aadc0df48e3299ca59
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-09-22 12:45:05 +0900

    feat: ログイン画面にstyled-componentsを適用

--- react_typescript/src/App.tsx
+++ react_typescript/src/App.tsx
@@ -1,12 +1,32 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./components/Login";
+import { Login } from "./components/Login";
 import Top from "./components/Top";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #0e101c;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={"/"} element={<Login />} />
+        <Route
+          path={"/"}
+          element={
+            <>
+              <GlobalStyle />
+              <Login />
+            </>
+          }
+        />
         <Route path={"/top"} element={<Top />} />
       </Routes>
     </BrowserRouter>
--- react_typescript/src/components/Login.tsx
+++ react_typescript/src/components/Login.tsx
@@ -1,9 +1,97 @@
 import { SubmitHandler, useForm } from "react-hook-form";
 import { LoginInputType } from "../type/LoginInputType";
-import "./Login.css";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
-const Login = () => {
+const StyledLoginPageWrapper = styled.div`
+  text-align: center;
+  color: white;
+  line-height: 2;
+  text-align: left;
+  display: block;
+  margin-bottom: 13px;
+  margin-top: 20px;
+  font-size: 14px;
+  font-weight: 200;
+`;
+
+const StyledLoginTitleContainer = styled.div`
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledForm = styled.form`
+  max-width: 500px;
+  margin: 0 auto;
+`;
+
+const StyledFormWrapper = styled.div`
+  background: #0e101c;
+  max-width: 400px;
+  margin: 0 auto;
+`;
+
+const StyledLabel = styled.label`
+  line-height: 2;
+  text-align: left;
+  display: block;
+  margin-bottom: 13px;
+  margin-top: 20px;
+  color: white;
+  font-size: 14px;
+  font-weight: 200;
+`;
+
+const StyledInput = styled.input`
+  display: block;
+  box-sizing: border-box;
+  width: 100%;
+  border-radius: 4px;
+  border: 1px solid white;
+  padding: 10px 15px;
+  margin-bottom: 10px;
+  font-size: 14px;
+
+  &:disabled {
+    opacity: 0.4;
+  }
+
+  &[type="submit"] {
+    background: #ec5990;
+    color: white;
+    text-transform: uppercase;
+    border: none;
+    margin-top: 40px;
+    padding: 15px;
+    font-size: 16px;
+    font-weight: 100;
+    letter-spacing: 5px;
+
+    &:hover {
+      background: #bf1650;
+    }
+
+    &:active {
+      transition: 0.3s all;
+      transform: translateY(3px);
+      border: 1px solid transparent;
+      opacity: 0.8;
+    }
+  }
+`;
+
+const StyledErrorMessage = styled.p`
+  color: #bf1650;
+
+  &::before {
+    display: inline;
+    content: "⚠ ";
+  }
+`;
+
+export const Login = () => {
   const {
     register,
     handleSubmit,
@@ -24,7 +112,7 @@ const Login = () => {
     if (data.accountId === "testUser" && data.password === "testpassword") {
       loginSuccess();
     } else {
-      alert("アカウントID or パスワードが間違っております");
+      alert("アカウントID or パスワードが間違っています");
     }
     reset();
   };
@@ -34,21 +122,32 @@ const Login = () => {
   };
 
   return (
-    <>
-      <div className="login-title-container">
+    <StyledLoginPageWrapper>
+      <StyledLoginTitleContainer>
         <h1>ログイン画面</h1>
-      </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label>アカウントID</label>
-        <input {...register("accountId", { required: true })} />
-        {errors.password && <p>アカウントIDを入力してください</p>}
-        <label>パスワード</label>
-        <input type="password" {...register("password", { required: true })} />
-        {errors.password && <p>パスワードを入力してください</p>}
-        <input type="submit" value={"ログイン"} />
-      </form>
-    </>
+      </StyledLoginTitleContainer>
+      <StyledFormWrapper>
+        <StyledForm onSubmit={handleSubmit(onSubmit)}>
+          <StyledLabel>アカウントID</StyledLabel>
+          <StyledInput {...register("accountId", { required: true })} />
+          {errors.password && (
+            <StyledErrorMessage>
+              アカウントIDを入力してください
+            </StyledErrorMessage>
+          )}
+          <StyledLabel>パスワード</StyledLabel>
+          <StyledInput
+            type="password"
+            {...register("password", { required: true })}
+          />
+          {errors.password && (
+            <StyledErrorMessage>
+              パスワードを入力してください
+            </StyledErrorMessage>
+          )}
+          <StyledInput type="submit" value={"ログイン"} />
+        </StyledForm>
+      </StyledFormWrapper>
+    </StyledLoginPageWrapper>
   );
 };
-
-export default Login;
