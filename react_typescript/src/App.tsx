aa30af2058e95d24736719b7c38ffd09b97d1756
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-10-29 19:12:49 +0900

    画像のパスの設定

--- react_typescript/src/App.tsx
+++ react_typescript/src/App.tsx
@@ -5,7 +5,7 @@ import { RecoilRoot } from "recoil";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background-image: url("/backgroundImage.jpg")
+    background-image: url("/react_typescript/public/backgroundImage.jpg")
   }
 `;
 
